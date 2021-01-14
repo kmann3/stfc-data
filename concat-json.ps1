49d805746db3ef8ba7af28d2ee5b3630573db5d2
Kennith Mann <kmann@etherpunk.com>
2021-01-14 12:18:15 -0600

    Writing script to merge json data.

--- concat-json.ps1
+++ concat-json.ps1
@@ -1,8 +1,34 @@
-$js1 = Get-Content -Path .\officer-one-of-ten.json -Raw |
-    ConvertFrom-Json
-$js2 = Get-Content -Path .\officer-TLaan.json -Raw |
-    ConvertFrom-Json
+#requires -version 2
+<#
+.SYNOPSIS
+  Script gets a list of officer, ship, and system JSON files.
+  It converts them to raw format.
+  Appends them into one massive variable.
+  When loop is done, outputs to concatenated data to a file.
+.DESCRIPTION
+  Merge all useful JSON data into one massive JSON file for easy transport or parsing.
+.INPUTS
+  None
+.OUTPUTS
+  Creates a compressed JSON file for the data.
+  Data is compressed because it will rarely, if ever, be read by a human.
+.NOTES
+  Version:        1.0
+  Author:         Kenny Mann
+  Creation Date:  2021-01-14
+  Purpose/Change: Initial script development
+  
+.EXAMPLE
+  > concat-json.ps1
+#>
 
-$js1 + $js2 |
-    ConvertTo-Json -Depth 5 |
-    Out-File -FilePath .\combinedfiles.json
+
+$allData = @()
+$fileList = Get-ChildItem -Name -Include officer*.json,ship*,json,system*.json -Attributes !Directory
+ForEach($file in $fileList) {
+    $data = Get-Content -Path $file -Raw | ConvertFrom-Json
+    Write-Output $data    
+    $allData += $data
+}
+
+$allData | ConvertTo-Json -Depth 20 -compress | Out-File -FilePath combinedFiles.json
