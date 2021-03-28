2dc124f7a7f099a9493ac5b145508475364c5f3f
Kennith Mann <kmann@etherpunk.com>
2021-03-28 15:51:29 -0500

    Updating officer json to be more easily converted.

--- STFC Web/Data/Entities/OfficerJson.cs
+++ STFC Web/Data/Entities/OfficerJson.cs
@@ -1,201 +1,113 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text.Json.Serialization;
-using System.Threading.Tasks;
 
 namespace STFC_Web.Data.Entities
 {
     // Root myDeserializedClass = JsonSerializer.Deserialize<Root>(myJsonResponse);
-    public class OfficerJSON
+
+    public partial class OfficerJson
     {
-        public class _1
-        {
-            [JsonPropertyName("level-range")]
-            public string LevelRange { get; set; }
+        [JsonProperty("author-comment")]
+        public string AuthorComment { get; set; }
 
-            [JsonPropertyName("name")]
-            public string Name { get; set; }
+        [JsonProperty("guid")]
+        public string Guid { get; set; }
 
-            [JsonPropertyName("shards-required")]
-            public int ShardsRequired { get; set; }
+        [JsonProperty("lastModified")]
+        public DateTimeOffset LastModified { get; set; }
 
-            [JsonPropertyName("resource-cost")]
-            public object ResourceCost { get; set; }
+        [JsonProperty("type")]
+        public string Type { get; set; }
 
-            [JsonPropertyName("xp")]
-            public int Xp { get; set; }
-        }
+        [JsonProperty("name")]
+        public string Name { get; set; }
 
-        public class _2
-        {
-            [JsonPropertyName("level-range")]
-            public string LevelRange { get; set; }
+        [JsonProperty("class")]
+        public string Class { get; set; }
 
-            [JsonPropertyName("name")]
-            public string Name { get; set; }
+        [JsonProperty("description")]
+        public string Description { get; set; }
 
-            [JsonPropertyName("shards-required")]
-            public int ShardsRequired { get; set; }
+        [JsonProperty("faction")]
+        public string Faction { get; set; }
 
-            [JsonPropertyName("resource-cost")]
-            public object ResourceCost { get; set; }
+        [JsonProperty("group")]
+        public string Group { get; set; }
 
-            [JsonPropertyName("xp")]
-            public int Xp { get; set; }
-        }
+        [JsonProperty("rarity")]
+        public string Rarity { get; set; }
 
-        public class _3
-        {
-            [JsonPropertyName("level-range")]
-            public string LevelRange { get; set; }
+        [JsonProperty("tags")]
+        public List<string> Tags { get; set; }
 
-            [JsonPropertyName("name")]
-            public string Name { get; set; }
+        [JsonProperty("synergy-command")]
+        public long SynergyCommand { get; set; }
 
-            [JsonPropertyName("shards-required")]
-            public int ShardsRequired { get; set; }
+        [JsonProperty("synergy-engineering")]
+        public long SynergyEngineering { get; set; }
 
-            [JsonPropertyName("resource-cost")]
-            public object ResourceCost { get; set; }
+        [JsonProperty("synergy-science")]
+        public long SynergyScience { get; set; }
 
-            [JsonPropertyName("xp")]
-            public int Xp { get; set; }
-        }
+        [JsonProperty("ranks")]
+        public Dictionary<string, Rank> Ranks { get; set; }
 
-        public class _4
-        {
-            [JsonPropertyName("level-range")]
-            public string LevelRange { get; set; }
+        [JsonProperty("captain-maneuver")]
+        public CaptainManeuver CaptainManeuver { get; set; }
 
-            [JsonPropertyName("name")]
-            public string Name { get; set; }
+        [JsonProperty("officer-ability")]
+        public OfficerAbility OfficerAbility { get; set; }
 
-            [JsonPropertyName("shards-required")]
-            public int ShardsRequired { get; set; }
+        [JsonProperty("image-url")]
+        public string ImageUrl { get; set; }
+    }
 
-            [JsonPropertyName("resource-cost")]
-            public object ResourceCost { get; set; }
+    public partial class CaptainManeuver
+    {
+        [JsonProperty("name")]
+        public string Name { get; set; }
 
-            [JsonPropertyName("xp")]
-            public int Xp { get; set; }
-        }
+        [JsonProperty("description:")]
+        public string Description { get; set; }
+    }
 
-        public class ResourceCost
-        {
-            [JsonPropertyName("federation-credits")]
-            public int FederationCredits { get; set; }
-        }
+    public partial class OfficerAbility
+    {
+        [JsonProperty("name")]
+        public string Name { get; set; }
 
-        public class _5
-        {
-            [JsonPropertyName("level-range")]
-            public string LevelRange { get; set; }
+        [JsonProperty("description")]
+        public string Description { get; set; }
 
-            [JsonPropertyName("name")]
-            public string Name { get; set; }
+        [JsonProperty("rank")]
+        public Dictionary<string, long> Rank { get; set; }
+    }
 
-            [JsonPropertyName("shards-required")]
-            public int ShardsRequired { get; set; }
+    public partial class Rank
+    {
+        [JsonProperty("level-range")]
+        public string LevelRange { get; set; }
 
-            [JsonPropertyName("resource-cost")]
-            public ResourceCost ResourceCost { get; set; }
+        [JsonProperty("name")]
+        public string Name { get; set; }
 
-            [JsonPropertyName("xp")]
-            public int Xp { get; set; }
-        }
+        [JsonProperty("shards-required")]
+        public long ShardsRequired { get; set; }
 
-        public class Rank
-        {
-            [JsonPropertyName("1")]
-            public _1 _1 { get; set; }
+        [JsonProperty("resource-cost")]
+        public List<ResourceCost> ResourceCost { get; set; }
 
-            [JsonPropertyName("2")]
-            public _2 _2 { get; set; }
+        [JsonProperty("xp")]
+        public long Xp { get; set; }
+    }
 
-            [JsonPropertyName("3")]
-            public _3 _3 { get; set; }
+    public partial class ResourceCost
+    {
+        [JsonProperty("type")]
+        public string Type { get; set; }
 
-            [JsonPropertyName("4")]
-            public _4 _4 { get; set; }
-
-            [JsonPropertyName("5")]
-            public _5 _5 { get; set; }
-        }
-
-        public class CaptainManeuver
-        {
-            [JsonPropertyName("name")]
-            public string Name { get; set; }
-
-            [JsonPropertyName("description:")]
-            public string Description { get; set; }
-        }
-
-        public class OfficerAbility
-        {
-            [JsonPropertyName("name")]
-            public string Name { get; set; }
-
-            [JsonPropertyName("description")]
-            public string Description { get; set; }
-
-            [JsonPropertyName("rank")]
-            public Rank Rank { get; set; }
-        }
-
-        public class Root
-        {
-            [JsonPropertyName("guid")]
-            public string Guid { get; set; }
-
-            [JsonPropertyName("lastModified")]
-            public DateTime LastModified { get; set; }
-
-            [JsonPropertyName("type")]
-            public string Type { get; set; }
-
-            [JsonPropertyName("name")]
-            public string Name { get; set; }
-
-            [JsonPropertyName("class")]
-            public string Class { get; set; }
-
-            [JsonPropertyName("description")]
-            public string Description { get; set; }
-
-            [JsonPropertyName("faction")]
-            public string Faction { get; set; }
-
-            [JsonPropertyName("group")]
-            public string Group { get; set; }
-
-            [JsonPropertyName("rarity")]
-            public string Rarity { get; set; }
-
-            [JsonPropertyName("tags")]
-            public List<string> Tags { get; set; }
-
-            [JsonPropertyName("synergy-command")]
-            public int SynergyCommand { get; set; }
-
-            [JsonPropertyName("synergy-engineering")]
-            public int SynergyEngineering { get; set; }
-
-            [JsonPropertyName("synergy-science")]
-            public int SynergyScience { get; set; }
-
-            [JsonPropertyName("rank")]
-            public Rank Rank { get; set; }
-
-            [JsonPropertyName("captain-maneuver")]
-            public CaptainManeuver CaptainManeuver { get; set; }
-
-            [JsonPropertyName("officer-ability")]
-            public OfficerAbility OfficerAbility { get; set; }
-
-            [JsonPropertyName("image-url")]
-            public string ImageUrl { get; set; }
-        }
+        [JsonProperty("count")]
+        public long Count { get; set; }
     }
 }
