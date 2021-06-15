63ceb31fd981f3f9b4a97e42da4a1668b85d2d5c
Kennith Mann <kmann@etherpunk.com>
2021-06-15 18:04:48 -0500

    Adding Service. Changing frontpage.

--- StfcWeb/Startup.cs
+++ StfcWeb/Startup.cs
@@ -35,7 +35,8 @@ namespace StfcWeb
             services.AddServerSideBlazor();
             services.AddScoped<AuthenticationStateProvider, RevalidatingIdentityAuthenticationStateProvider<IdentityUser>>();
             services.AddDatabaseDeveloperPageExceptionFilter();
-            services.AddSingleton<WeatherForecastService>();
+            //services.AddSingleton<WeatherForecastService>();
+            services.AddSingleton<EntityServices>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
