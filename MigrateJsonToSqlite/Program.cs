41a91b109ad41ff2451f10663be474ccdc3611c8
Kennith Mann <kmann@etherpunk.com>
2021-06-22 06:59:51 -0500

    Adding temp project to help with migration to sqlite and then to sqlserver. sqlite will be downloadable db

---
+++ MigrateJsonToSqlite/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MigrateJsonToSqlite
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
